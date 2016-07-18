7f47cbc7c86d10ac9fbe6bcbbf97e4318c183e91
zblfendou <740222820@qq.com>
2016-07-18 17:22:12 +0800

    修复bug

--- src/main/java/cn/track/controller/UserAjaxController.java
+++ src/main/java/cn/track/controller/UserAjaxController.java
@@ -1,10 +1,19 @@
 package cn.track.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrator on 2016.6.23.
  */
 @Controller
 public class UserAjaxController {
+
+	@RequestMapping ("/test.do")
+	public ModelAndView test (@RequestParam ("name") String name) {
+		System.out.println ("测试成功" + name);
+		return new ModelAndView ("aaa");
+	}
 }
+
---
+++ src/main/webapp/WEB-INF/view/aaa.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2016.7.18
+  Time: 15:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>测试成功</title>
+</head>
+<body>
+测试成功1111111111111111
+</body>
+</html>
