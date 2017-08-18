3802b4ab506102ebb26d9bd1703574d9dbe29e3c
zblfendou <zhangbinleifendou@163.com>
2017-08-18 16:29:25 +0800

    正确提交

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -1,9 +1,15 @@
 package base;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2016.6.21.
@@ -12,4 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration ({"classpath:application-context.xml", "classpath:application-amqp.xml", "classpath:application-quartz.xml"})
 @Rollback (false)
 public class Base {
+    @Test
+    public void ignoreTest() {
+        System.out.println("test");
+    }
 }
